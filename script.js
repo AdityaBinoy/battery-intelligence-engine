35e7bbb09a82e345279be91082f08d2ccb1bfda0
AdityaBinoy <k.aditya1307@gmail.com>
2026-06-21 13:37:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,77 +1,63 @@
-function updateBattery() {
-
-let cells = [];
-
-for(let i=0;i<4;i++){
-cells.push((3.8 + Math.random()*0.4));
+body{
+font-family:Segoe UI,sans-serif;
+background:#0f172a;
+color:white;
+margin:0;
+padding:20px;
 }
 
-document.getElementById("c1").innerHTML =
-cells[0].toFixed(2)+" V";
-
-document.getElementById("c2").innerHTML =
-cells[1].toFixed(2)+" V";
-
-document.getElementById("c3").innerHTML =
-cells[2].toFixed(2)+" V";
-
-document.getElementById("c4").innerHTML =
-cells[3].toFixed(2)+" V";
-
-let avg =
-cells.reduce((a,b)=>a+b,0)/4;
-
-let max =
-Math.max(...cells);
-
-let min =
-Math.min(...cells);
-
-let imbalance =
-((max-min)/avg)*100;
-
-let strongest =
-cells.indexOf(max)+1;
-
-let weakest =
-cells.indexOf(min)+1;
-
-let status;
-let css;
-
-if(imbalance < 2){
-status="Healthy";
-css="healthy";
-}
-else if(imbalance < 5){
-status="Minor Imbalance";
-css="minor";
-}
-else if(imbalance < 10){
-status="Critical Imbalance";
-css="critical";
-}
-else{
-status="Pack Failure";
-css="failure";
+.container{
+max-width:1100px;
+margin:auto;
 }
 
-document.getElementById("avg").innerHTML =
-avg.toFixed(2)+" V";
-
-document.getElementById("imb").innerHTML =
-imbalance.toFixed(2)+" %";
-
-document.getElementById("strong").innerHTML =
-"Cell "+strongest;
-
-document.getElementById("weak").innerHTML =
-"Cell "+weakest;
-
-document.getElementById("status").innerHTML =
-`<span class="${css}">${status}</span>`;
+h1{
+text-align:center;
+margin-bottom:30px;
 }
 
-updateBattery();
+.grid{
+display:grid;
+grid-template-columns:repeat(auto-fit,minmax(200px,1fr));
+gap:20px;
+}
 
-setInterval(updateBattery,1000);
+.card{
+background:#1e293b;
+padding:20px;
+border-radius:15px;
+text-align:center;
+box-shadow:0 0 10px rgba(0,0,0,.4);
+}
+
+.summary{
+margin-top:30px;
+background:#1e293b;
+padding:25px;
+border-radius:15px;
+}
+
+hr{
+margin:40px 0;
+border:1px solid #334155;
+}
+
+.healthy{
+color:#22c55e;
+font-weight:bold;
+}
+
+.minor{
+color:#eab308;
+font-weight:bold;
+}
+
+.critical{
+color:#f97316;
+font-weight:bold;
+}
+
+.failure{
+color:#ef4444;
+font-weight:bold;
+}
